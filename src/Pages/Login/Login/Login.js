a396d59c60e85cdd1e97afd4d46e46885864e653
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-07 09:44:31 +0600

    login page added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,17 +1,31 @@
 import React, { useRef } from 'react';
 import './Login.css'
 import { useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from './../../../firebase.init';
 
 const Login = () => {
   const emailRef     = useRef('');
   const passwordRef  = useRef('');
   const navigate     = useNavigate()
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if(user){
+    navigate('/');
+  }
+
   const handleSubmit = event => {
     event.preventDefault();
     const email    = emailRef.current.value;
     const password = passwordRef.current.value;
-    console.log(email, password);
+
+    signInWithEmailAndPassword(email, password);
   }
 
   const handleRegister = event =>{
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from './../../../firebase.init';
 
 const Register = () => {
+    const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const handleLogin = () =>{
         navigate('/login')
     }
+    
+    if(user){
+      navigate('/');
+  }
 
     const handleRegister = event =>{
         event.preventDefault();
+        // console.log(event.target.email.value);
+        // console.log(event.target.password.value);
+        const name     = event.target.name.value;
+        const email    = event.target.email.value;
+        const password = event.target.password.value;
+
+
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div>
